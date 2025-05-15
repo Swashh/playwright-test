135b43b143b7e0011bbf6d4fa23e0c93b18fcc88
Swashh <k.serewka@gmail.com>
2025-05-15 22:18:36 +0300

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: "html",
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 
   timeout: 30_000,
@@ -59,4 +59,10 @@ export default defineConfig({
       },
     },
   ],
+
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });

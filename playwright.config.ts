98e83be8ae1c2b2042afaf78221c51958fa0a2f2
Swashh <k.serewka@gmail.com>
2025-05-16 12:14:25 +0300

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
 
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://google.com',
+    baseURL: 'https://klr.com.ua',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
